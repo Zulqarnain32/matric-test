d618b6651b886c9562c44c28073941f4a779538a
Zulqarnain <zulqarnainchohan250@gmail.com>
2025-06-08 03:56:10 -0700

    added atlas strong connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,27 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const dbConnect = () => {
-    // mongoose.connect("mongodb://127.0.0.1:27017/matric")
-  
-//   zsIO8lAaSjXJlIdS
-    mongoose.connect("mongodb+srv://zulqarnainc67:zsIO8lAaSjXJlIdS@cluster0.z0diyyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        .then(async () => {
-            console.log("Connected successfully to MongoDB");
-        })
-        .catch((err) => {
-            console.error("MongoDB Connection Error:", err);
-        });
+let isConnected = false;
+
+const dbConnect = async () => {
+  if (isConnected) {
+    console.log("✅ Using existing MongoDB connection");
+    return;
+  }
+// zsIO8lAaSjXJlIdS
+  try {
+   
+    const db = await mongoose.connect("mongodb+srv://zulqarnainc67:zsIO8lAaSjXJlIdS@cluster0.z0diyyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+
+
+    
+
+    isConnected = db.connections[0].readyState === 1;
+    console.log("✅ New MongoDB connection established");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Error:", err);
+    throw err;
+  }
 };
-
 
 module.exports = dbConnect;
