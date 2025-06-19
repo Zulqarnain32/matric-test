133045cbeb47336f2046a54996a6816c2c5b0238
Zulqarnain <zulqarnainchohan250@gmail.com>
2025-06-19 01:18:54 -0700

    added mcqs

--- server/seedQuestions.js
+++ server/seedQuestions.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const dbConnect = require("./config/db");
 const Question = require("./models/QuestionModel");
 
@@ -21,22 +20,96 @@ const questions = [
       "What is the key advantage of the Von Neumann architecture?",
       "What are the three main requirements for a computing system to function?",
     ],
-    longQuestions: ["Explain the main components of a computer with examples."],
+    longQuestions: [
+      "Define and describe the concept of system. Explain the fundamental components, objectives, environment, and methods of communication within a system.",
+      "Differentiate between natural and artificial systems. Discuss their characteristics, functions and purposes with relevant examples.",
+      "Examine in the relationship between systems and different branches of science, including natural science, design science and computer science. How do these branches utilize system theory to understand and improve their respective fields? Provide specific examples to support your analysis.",
+      "Explore the different types of computing systems such as computers, software systems, computer network and the internet.",
+      "Describe in the main characteristics of a computer as a system, including its objectives, components and interactions among these components.",
+      "Explain the Von Neumann architecture of a computer. Include a discussion on the main components, their functions and the step-by-step process of how the architecture operates.",
+      "Provide an detailed explanation of how a computer interacts with its environment. Include examples of user input, network communication, and power supply.",
+      "Describe the process of retrieving and displaying an file using. a computer based on the information among different components. Provide a step-by-step explanation of how input its processed, data is transferred, and results are displayed on the screen.",
+    ],
     mcqs: [
       {
-        question: "Which one is an input device?",
-        options: ["Monitor", "Keyboard", "Printer", "Speaker"],
-        answer: "Keyboard",
+        question: "What is the primary function of a system?",
+        options: [
+          "To work independently",
+          "To achieve a common goal",
+          "To create new entertainment",
+          "To provide information",
+        ],
       },
       {
-        question: "What does CPU stand for?",
+        question: "What is one of the fundamental concept of any system?",
+        options: ["Its size", "Its objective", "Its age", "Its price"],
+      },
+      {
+        question: "What is an example of a simple system?",
         options: [
-          "Central Processing Unit",
-          "Control Panel Unit",
-          "Central Power Unit",
-          "Central Processing Utility",
+          " A human body",
+          "A computer network",
+          "A thermostat regulating temperature",
+          "The internet",
         ],
-        answer: "Central Processing Unit",
+      },
+      {
+        question:
+          "What type of the environment remains unchanged unless the system provides an output?",
+        options: ["Dynamic", "Static", "Deterministic", "Non-deterministic"],
+      },
+      {
+        question: "What are the basic components of a system?",
+        options: [
+          "Users, hardware, software",
+          "Objectives, components, communication",
+          "Inputs, outputs, processes",
+          "Sensors, actuators, controllers",
+        ],
+      },
+      {
+        question: "What concept does the theory of system aim to understand?",
+        options: [
+          "  Hardware design",
+          "  System interactions and development over time",
+          "Software applications",
+          " Network Security",
+        ],
+      },
+      {
+        question:
+          "What role does the operating system (OS) play in a computer?",
+        options: [
+          "It performs calculations and executes",
+          "It temporarily stores data and instructions for the CPU",
+          "It receives inputs from storage components and decides what to do with it",
+          "It provides long-term storage of data and software",
+        ],
+      },
+      {
+        question:
+          "Which describe the following are the Von Neumann architecture's main characteristic?",
+        options: [
+          "Separate memory of data",
+          "Parallel execution of instructions",
+          "Single memory for both program instructions and data",
+          "Lack of flexibility in executing instructions",
+        ],
+      },
+
+      {
+        question: "What is a disadvantage of the Von Neumann architecture?",
+        options: [
+          "Complex design due to separate memory spaces",
+          "Bottleneck due to program stored in memory",
+          "Difficult to modify single memory space for instructions and data",
+          "Lack of flexibility in executing instructions",
+        ],
+      },
+      {
+        question:
+          "Which of the following transports data inside a computer among different components?",
+        options: ["Control unit", "System Bus", "Memory", "Processor"],
       },
     ],
   },
@@ -59,23 +132,83 @@ const questions = [
       "Why is it important to understand the limitations of floating-point representation in scientific computing?",
     ],
     longQuestions: [
-      "Describe the major components of the CPU and their functions.",
+      "Explain how  characters are encoded using Unicode. Provide examples of characters form different languages and their corresponding Unicode code points.",
+      "Describe in detail how integers are stored in computer memory.",
+      "Explain in the the process of converting decimal integer to its binary representation and vise versa. Include examples or both positive and negative integers.",
+      "Perform in the following binary arithmetic operations: (a) Multiplication of 101 by 11. (b) Division of 100 by 10.",
+      "Add the following binary numbers: (a) 101 + 110 (b) 1100 + 1011.",
+      "Convert the following numbers to: A 4-bit binary and add them: (a) 7 - (-4) (b) 4 - 5 + 3.",
     ],
     mcqs: [
       {
-        question: "Which one is an input device?",
-        options: ["Monitor", "Keyboard", "Printer", "Speaker"],
-        answer: "Keyboard",
+        question: "What does ASCII stands for?",
+        options: [
+          "American Standard Code for Information Interchange",
+          "American Standard Information Communication Interface",
+          "American Standard Communication for Information Interchange",
+          "Advanced Standard Communication for Information Interchange",
+        ],
       },
       {
-        question: "What does CPU stand for?",
+        question: "Which if the the following number is a valid binary number?",
+        options: ["1101102", "11011", "110.11", "1101A"],
+      },
+
+      {
+        question: "How many bits are used in the standard ASCII encoding?",
+        options: ["7 bits", "8 bits", "16 bits", "32 bits"],
+      },
+      {
+        question:
+          "Which of the the following is a key advantage of Unicode over ASCII?",
         options: [
-          "Central Processing Unit",
-          "Control Panel Unit",
-          "Central Power Unit",
-          "Central Processing Utility",
+          "It used fewer bits per character",
+          "It can represents characters for many different languages",
+          "It is backward compatible with binary",
+          "It is specific to the English language",
         ],
-        answer: "Central Processing Unit",
+      },
+      {
+        question: "How many bytes are use to store a typical integer?",
+        options: ["1 byte", "2 bytes", "4 bytes", "8 bytes"],
+      },
+      {
+        question:
+          "What is the primary difference between signed and unsigned integers?",
+        options: [
+          "Unsigned integers cannot be negative",
+          "Signed integers have a large range",
+          "Unsigned integers are stored in floating-point format",
+          "Signed integers are only used for positive numbers",
+        ],
+      },
+      {
+        question:
+          "In the single precision, how many bits are used for the exponent?",
+        options: ["23 bits", "8 bits", "11 bits", "52 bits"],
+      },
+      {
+        question:
+          "What is the approximate range of values for single-precision floating-point numbers?",
+        options: [
+          "1.4 x 10^-45 to 3.4 x 10^38",
+          "1.4 x 10^-38 to 3.4 x 10^45",
+          "1.0 x 10^-324 to 1.8 x 10^308",
+          "1.0 x 10^308 to 1.8 x 10^324",
+        ],
+      },
+      {
+        question: "What are the tiny dots that make up an image called?",
+        options: ["Pixels", "Bits", "Bytes", "Nodes"],
+      },
+      {
+        question: "In an RGB color model, what does RGB stand for?",
+        options: [
+          "Red, Green, Blue",
+          "Red, Grey, Black",
+          "Right, Green, Blue",
+          "Red, Green, Brown",
+        ],
       },
     ],
   },
